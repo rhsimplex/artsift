3f41ecf4ca0bf8771d74472c3e2027ac4f4e8082
rhsimplex <hendersonry@gmail.com>
2014-09-22 13:25:39 +0200

    fixed some things...

--- art_utils.py
+++ art_utils.py
@@ -10,7 +10,7 @@ def repeated_sales(df, artistname, artname, r2thresh=7000, fftr2thresh=10000, IM
     """
         Takes a dataframe, artistname and artname and tries to decide, via image matching, if there is a repeat sale. Returns a dict of lot_ids, each entry a list of repeat sales
     """
-    artdf = df[(df['artistName']==artistname) & (df['artTitle']==artname)]
+    artdf = df[(df['artistID']==artistname) & (df['artTitle']==artname)]
     def getpath(x):
         if len(x) > 0:
             return x[0]['path']
@@ -19,20 +19,21 @@ def repeated_sales(df, artistname, artname, r2thresh=7000, fftr2thresh=10000, IM
 
     artdf.images = artdf.images.apply(getpath)
     paths = artdf[['_id','images']].dropna()
-    num_records = paths.shape[0]
     id_dict = {}
     img_buffer = {}
+    already_ordered = []
     for i, path_i in paths.values:
         id_dict[i] = []
         img_buffer[i] = img_as_float(rgb2gray(resize(imread(IMAGES_DIR + path_i), (300,300))))
         for j, path_j in paths[paths._id != i].values:
-            if j > i:
-                if j in img_buffer.keys():
-                    image_test = img_buffer[j]
-                else:
+            if j > i and j not in already_ordered:
+                if j not in img_buffer.keys():
                     img_buffer[j] = img_as_float(rgb2gray(resize(imread(IMAGES_DIR + path_j), (300,300))))
                 if norm(img_buffer[i] - img_buffer[j]) < r2thresh and\
                         norm(fft2(img_buffer[i]) - fft2(img_buffer[j])) < fftr2thresh:
                     id_dict[i].append(j)
-                
-    return id_dict 
+                    already_ordered.append(j)
+    for key in id_dict.keys():
+        if id_dict[key] == []:
+            id_dict.pop(key)
+    return id_dict
